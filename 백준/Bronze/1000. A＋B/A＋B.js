98e0aa03e3b1f73eb1d9c61951f84588c92dc8e3
TFOJL <108859281+dle234@users.noreply.github.com>
2024-12-23 22:07:48 +0900

    [Bronze V] Title: A+B, Time: 100 ms, Memory: 9344 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -0,0 +1,7 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim().split("\n");
+// "/dev/stdin"
+// "test/Test.txt"
+const [a, b] = input[0].split(" ").map(Number);
+
+console.log(a + b);
